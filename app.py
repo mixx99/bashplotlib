bf55cbc11395fff0e3d413d8c4f56130d2f8b1c2
mixx99 <maxkriv1@gmail.com>
2025-05-09 09:50:46 +0300

    first task

---
+++ app.py
@@ -0,0 +1,18 @@
+# scratch.py
+from bashplotlib.scatterplot import plot_scatter
+
+x_coords = [-10,20,30]
+y_coords = [-10,20,30]
+width = 10
+char = 'x'
+color = 'default'
+title = 'My Test Graph'
+
+plot_scatter(
+    None,
+    x_coords,
+    y_coords,
+    width,
+    char,
+    color,
+    title)
